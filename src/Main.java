1869c3d1d13a23584b5d03f6c899fd50fd3a608e
KonstantinPos <posusaevkostay@mail.ru>
2017-12-13 10:29:51 +0300

    Task4

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double a = 44.24;
+        int result = (int) Math.round(a);
+        System.out.println("Оклуглив " +a +" получим "+result);
+    }
+}
